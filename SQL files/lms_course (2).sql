1c3b686a306d0d7042d4f5f68d05106fba5678ae
Tananatham <73059344+Tananatham@users.noreply.github.com>
2021-11-02 14:32:41 +0800

    updated SQL file

--- SQL files/lms_course (2).sql
+++ SQL files/lms_course (2).sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `lms_course`
 --
-CREATE DATABASE IF NOT EXISTS `lms_course` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+DROP DATABASE IF EXISTS `lms_course`;
+CREATE DATABASE IF NOT EXISTS `lms_course` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `lms_course`;
 
 -- --------------------------------------------------------
@@ -191,13 +192,13 @@ CREATE TABLE IF NOT EXISTS `lesson` (
   `class_id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
   `quiz_id` int(11) NOT NULL,
-  `lesson_descriptions` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `lesson_descriptions` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `lesson_name` varchar(50) NOT NULL,
   `quiz_type` varchar(50) NOT NULL,
   `lesson_material` varchar(100) DEFAULT NULL,
   `created_on` varchar(100) NOT NULL,
   PRIMARY KEY (`lesson_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `lesson`
@@ -224,7 +225,7 @@ CREATE TABLE IF NOT EXISTS `quiz` (
   `quiz_id` int(11) NOT NULL AUTO_INCREMENT,
   `quiz_type` varchar(10) NOT NULL,
   `lesson_id` int(11) NOT NULL,
-  `quiz_question` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `quiz_question` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `datetime_created` varchar(100) NOT NULL,
   `passing_score` int(3) DEFAULT NULL,
   `correct_answer` varchar(100) NOT NULL,
@@ -232,7 +233,7 @@ CREATE TABLE IF NOT EXISTS `quiz` (
   `time_limit` varchar(50) NOT NULL,
   PRIMARY KEY (`quiz_id`),
   KEY `lesson_id` (`lesson_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Constraints for dumped tables
@@ -241,9 +242,12 @@ CREATE TABLE IF NOT EXISTS `quiz` (
 --
 -- Constraints for table `quiz`
 --
+
 ALTER TABLE `quiz`
   ADD CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `lesson` (`lesson_id`);
 COMMIT;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

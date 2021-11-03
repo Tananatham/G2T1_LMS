84401bb77c15bbd8d0bf06e5bc910f7d82fa53a6
alina <alinaatxn@gmail.com>
2021-11-03 11:47:47 +0800

    updated integrationtest.py

--- test_intergration.py
+++ test_intergration.py
@@ -1,8 +1,8 @@
 import unittest   
-#import flask_testing
+import flask_testing
 import json
 
-from course import app,db, Employee, Course, Class, Lesson, Course_check, PrerequisiteCheck, Quiz
+from course import app,db, Employee, Course, Class, Lesson,Quiz
 
 
 # class TestApp(flask_testing.TestCase):
@@ -16,16 +16,15 @@ from course import app,db, Employee, Course, Class, Lesson, Course_check, Prereq
 #     def setUp(self):
 #         db.create_all()
 
-#     def tearDown(self):
-#         db.session.remove()
-#         db.drop_all()
+# #     def tearDown(self):
+# #         db.session.remove()
+# #         db.drop_all()
 
 
 #Author: Chelsea
-class TestEmployee(unittest.TestCase):
-    def setUp(self):
-        # Load test data
-        self.app = Employee(database='fixtures/test_basic.json')
+class TestEmployee(TestApp):
+    def test_employee_course_prerequisite_check(self):
+       self.assertEqual(4,4)
 
     def test_name_lookup_employee(self):
       self.assertEqual(4,4)
@@ -35,18 +34,17 @@ class TestEmployee(unittest.TestCase):
 
     def test_find_inprogress_class_id(self):
        self.assertEqual(4,4)
-
+       
     def test_find_employee_in_progress(self):
        self.assertEqual(4,4)
 
     def test_delete_status(self):
         self.assertEqual(4,4)
 
-    def test_employee_course_prerequisite_check(self):
-       self.assertEqual(4,4)
-
-#Author: 
-class TestCourse(unittest.TestCase):
+#Author: Tantham 
+class TestCourse(TestApp):
+    def test_course_prereq_okay_check(self):
+        self.assertEqual(4, 4)
 
     def test_set_course_prereq(self):
         self.assertEqual(4,4)
@@ -57,32 +55,38 @@ class TestCourse(unittest.TestCase):
     def test_find_course_name_by_id(self):
         self.assertEqual(4,4)
 
-    def test_course_prereq_okay_check(self):
-        self.assertEqual(4, 4)
+    def test_get_pending_enrollment(self):
+        self.assertEqual(4,4)
+    
+    def test_get_in_progress_enrollment(self):
+        self.assertEqual(4,4)
 
 
 #Author: Alina Tan 
-class TestClass(unittest.TestCase):
+class TestClass(TestApp):
     def test_create_status(self):
         self.assertEqual(4, 4)
 
     def test_update_status(self):
         self.assertEqual(4,4)
 
-#Author: 
-class TestQuiz(unittest.TestCase):
+#Author: Brenda
+class TestQuiz(TestApp):
     def test_get_all_quiz(self):
         self.assertEqual(4, 4)
     
-    def test_get_quiz_by_lesson_id(self):
-        self.assertEqual(4, 4)
-    
-    def test_find_by_lessonid(self):
+    def test_find_quiz_by_lesson_id(self):
         self.assertEqual(4, 4)
 
-#Author: 
-class TestLesson(unittest.TestCase):
-    def test_find_quiz_by_lesson_id(self):
+    def test_create_quiz(self):
+         self.assertEqual(4, 4)
+
+#Author: Frank
+class TestLesson(TestApp):
+    def test_find_lesson_by_class_id(self):
+        self.assertEqual(4, 4)
+
+    def test_find_course_material_by_lesson_id(self):
         self.assertEqual(4, 4)
 
 if __name__ == '__main__':

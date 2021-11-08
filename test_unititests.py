67b7b9cd159a2cf574558b2baee84172ab50bef9
Tananatham <73059344+Tananatham@users.noreply.github.com>
2021-11-08 17:51:30 +0800

    edit unit test

--- test_unititests.py
+++ test_unititests.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import unittest   
 from course import Employee, Course, Class, Lesson, Course_check, PrerequisiteCheck, Quiz
 
-#Author: Chelsea
+# Author: Chelsea
 class TestEmployee(unittest.TestCase):
     def test_to_dict(self):
         e1 = Employee(employee_id = '1', course_id = '1', employee_name='James', employee_role='Engineer')
@@ -13,7 +13,7 @@ class TestEmployee(unittest.TestCase):
             'employee_role': 'Engineer'}
         )
 
-#Author: Tantham 
+# Author: Tantham 
 class TestCourse(unittest.TestCase):
     def test_to_dict(self):
         c1 = Course(course_id='1', course_name='PlaceHolder', total_no_of_class='5', total_no_of_lesson='2', class_id='1', course_description='Fix', course_prerequisite='2', coursem_id='2', employee_id='1', start_time='Now', end_time='Later', datetime_uploaded='2021-09-14 00:00:00', start_enrol='2021-09-14', end_enrol='2021-09-14')
@@ -34,7 +34,7 @@ class TestCourse(unittest.TestCase):
             'end_enrol': '2021-09-14'}
         )
 
-#Author: Alina Tan 
+# Author: Alina Tan 
 class TestClass(unittest.TestCase):
     def test_class_enrol(self):
         class1 = Class(class_id = '1', course_id='1', lesson_id = '1', course_name = 'PlaceHolder', start_date = 'Start', end_date = 'End', start_time = 'Start', end_time = 'End', class_size = 23, current_class_size = 4, employee_id = '5', duration_of_class ='5')
@@ -64,7 +64,7 @@ class TestClass(unittest.TestCase):
             self.assertEqual(str(e), 'Class size cannot be less than zero.')
             self.assertEqual(class1.current_class_size, 1)
 
-#Author: Brenda
+# Author: Brenda
 class TestCourseCheck(unittest.TestCase):
     def test_to_dict(self):
         checkcourse = Course_check(employee_id = '1', course_id = '4', class_id='2', status='in-progress')
@@ -75,7 +75,7 @@ class TestCourseCheck(unittest.TestCase):
             'status': 'in-progress'}
         )
 
-#Author: Frank
+# Author: Frank
 class TestPrerequisiteCheck(unittest.TestCase):
     def test_to_dict(self):
         checkprereq = PrerequisiteCheck(course_id='2', prerequisite_course_id='5')

1f7cef3814e75f11595473985219f002416f3c59
brendachong2019 <brendachong.2019@smu.edu.sg>
2021-11-09 14:16:10 +0800

    update unit and integartion testing

--- test_intergration.py
+++ test_intergration.py
@@ -204,49 +204,25 @@ class TestClass(TestApp):
         self.assertEqual(response.json['code'], 201)            
         self.assertEqual(response.json['data']['status'], 'in-progress')      
 
-
-    # def test_update_status(self):
-    #     e1 = Employee(employee_id = 1, course_id = 4, employee_name='James', employee_role='Engineer')
-    #     c1 = Course(course_id = 4, course_name = 'Chemical engineering', total_no_of_class = 3, total_no_of_lesson = 2, class_id = 2, course_description = 'Civil engineering is the professional practice of designing and developing infrastructure projects. This can be on a huge scale, such as the development of nationwide transport systems or water supply networks, or on a smaller scale, such as the development of single roads or buildings.', course_prerequisite = 0, coursem_id = 1, employee_id = 2, start_time = '5 August 2021', end_time = '1 December 2021', datetime_uploaded = '2021-08-17 00:00:00', start_enrol= '2021-06-23', end_enrol= '2021-06-23')
-    #     class2 = Class(class_id = 2, course_id=4, lesson_id = 3, course_name = 'Civil Engineer course', start_date = '01/09/2021', end_date = '01/12/2021', start_time = '14:00', end_time = '16:00', class_size = 10, current_class_size = 5, employee_id = 1, duration_of_class =2)
-    #     status = 'completed'
-    #     db.session.commit()
-
-    #     request_body = {
-    #         'employee_id': e1.employee_id,
-    #         'course_id' : c1.course_id,
-    #         'class_id' : class2.class_id,
-    #         'status' : status
-    #     }
-        
-    #     response = self.client.put("/employee_course_status",
-    #                                 data=json.dumps(request_body),
-    #                                 content_type='application/json') 
-    #     self.assertEqual(response.json['code'], 200)     
-    #     self.assertEqual(response.json['data']['status'],     
-    #     {
-    #         'status': 'completed'
-    #     })      
-
-    # def test_find_classes_by_course_id(self):
-    #     class2 = Class(class_id = 2, course_id=4, lesson_id = 3, course_name = 'Civil Engineer course', start_date = '01/09/2021', end_date = '01/12/2021', start_time = '14:00', end_time = '16:00', class_size = 10, current_class_size = 5, employee_id = 1, duration_of_class =2)
-    #     db.session.add(class2)
-    #     db.session.commit()
-    #     response = self.client.get("/class_by_course_id/4")
-    #     self.assertEqual(response.json['code'], 200)
-    #     self.assertEqual(response.json['data']['class'], 
-    #     [{'class_id': 2, 'course_id': 4,  'lesson_id' : 3,
-    #         'course_name' : 'Civil Engineer course',
-    #         'start_date': '01/09/2021',
-    #         'end_date': '01/12/2021',
-    #         'start_time': '14:00',
-    #         'end_time': '16:00',
-    #         'class_size': 10,
-    #         'current_class_size': 5,
-    #         'employee_id': 1,
-    #         'duration_of_class': 2}
+    def test_find_classes_by_course_id(self):
+        class2 = Class(class_id = 2, course_id=4, lesson_id = 3, course_name = 'Civil Engineer course', start_date = '01/09/2021', end_date = '01/12/2021', start_time = '14:00', end_time = '16:00', class_size = 10, current_class_size = 5, employee_id = 1, duration_of_class =2)
+        db.session.add(class2)
+        db.session.commit()
+        response = self.client.get("/class_by_course_id/4")
+        self.assertEqual(response.json['code'], 200)
+        self.assertEqual(response.json['data']['class'], 
+        [{'class_id': 2, 'course_id': 4,  'lesson_id' : 3,
+            'course_name' : 'Civil Engineer course',
+            'start_date': '01/09/2021',
+            'end_date': '01/12/2021',
+            'start_time': '14:00',
+            'end_time': '16:00',
+            'class_size': 10,
+            'current_class_size': 5,
+            'employee_id': 1,
+            'duration_of_class': 2}
             
-    #         ])
+            ])
 
     def test_create_class(self):
         request_body = {
